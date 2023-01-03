865d6ce9d3877ade91d26636c4ade6fcba019d72
bolom <bolomozhnov@gmail.com>
2023-01-03 20:07:36 +0300

    reDone2.0

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,18 @@
 import Transport.Bus;
 import Transport.Car;
 
-import java.sql.SQLOutput;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        Bus bus1 = new Bus("Black",15);
+       Bus bus1 = new Bus("Volvo","XC60","Russia",2023,850,"White");
         System.out.println(bus1.toString());
 
-        Bus bus2 = new Bus("Red", 45);
-        System.out.println(bus2.toString());
 
-        Bus bus3 = new Bus("Orange", 32);
-        System.out.println(bus3.toString());
-
-        Car car1 = new Car("Audi","A6","black",25, 1.8F,"",23,true);
+        Car car1 = new Car("Audi","A6","Germany",2000,60,"Black",1.8F,"","Sedan",23,6,true,8);
         car1.getBrand();
 
-        car1.setBRAND("BMW");
+        car1.setBrand("BMW");
         System.out.println(car1.toString());
 
     }
--- src/Transport/Bus.java
+++ src/Transport/Bus.java
@@ -1,14 +1,15 @@
 package Transport;
 public class Bus extends Transport {
-    public Bus(String colour, int speed) {
-        super(colour, speed);
+    public Bus(String brand, String model, String prodCountry, int prodYear, int speed, String colour) {
+        super(brand, model, prodCountry, prodYear, speed, colour);
     }
+
     @Override
     public String toString() {
-        return  "Марка машины " + BRAND + "\n"
-                + "Модель " + MODEL + "\n"
-                + "Страна производитель " + PROD_COUNTRY + "\n"
-                + "Год выпуска " + PROD_YEAR + "\n"
+        return  "Марка машины " + brand + "\n"
+                + "Модель " + model + "\n"
+                + "Страна производитель " + prodCountry + "\n"
+                + "Год выпуска " + prodYear + "\n"
                 + "Цвет " + colour + "\n"
                 + "Скорость " + speed + "\n";
     }
